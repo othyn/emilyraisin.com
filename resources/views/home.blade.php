730fd92bcf06d982adb51dffe60abbb01444c5fe
Ben <git@othyn.com>
2019-01-06 16:04:41 +0000

    Update image

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12 h-100 d-flex flex-column justify-content-center text-center">
             <div class="row">
                 <div class="col-12 mb-4">
-                    <img src="https://media.licdn.com/dms/image/C5603AQH63ljMbu0Ocg/profile-displayphoto-shrink_800_800/0?e=1551916800&v=beta&t=znztYPKKOmUui3xTzmH4WwF1CKTHzxOiHsrrtGzKwyQ" class="img-thumbnail rounded-circle img-fluid mx-auto d-block img-ruled" alt="Me!">
+                    <img src="/img/me.jpeg" class="img-thumbnail rounded-circle img-fluid mx-auto d-block img-ruled" alt="Me!">
                 </div>
                 <div class="col-12 mb-4">
                     <h1>Excellant freelance copyrighter with a<br>nack for proofreeding.</h1>
