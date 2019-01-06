1a20011f79de689136d5d14944c5d83cac25fa9b
Ben <git@othyn.com>
2019-01-06 16:05:10 +0000

    Little bit of movement on pages

--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -8,6 +8,7 @@
             <div class="row">
                 <div class="col-12">
                     <h1 class="animated fadeInDown">Applying the finishing touches!</h1>
+                    <a class="d-block animated fadeInUp" href="/">Take me home</a>
                 </div>
             </div>
         </div>
