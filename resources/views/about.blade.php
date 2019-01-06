cb55f82ddab2592c8600f9ef020005ff917e3add
Ben <git@othyn.com>
2019-01-06 15:26:05 +0000

    Apply placeholder animation

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -7,7 +7,7 @@
         <div class="col-12 h-100 d-flex flex-column justify-content-center text-center">
             <div class="row">
                 <div class="col-12">
-                    <h1>Applying the finishing touches!</h1>
+                    <h1 class="animated fadeInDown">Applying the finishing touches!</h1>
                 </div>
             </div>
         </div>
