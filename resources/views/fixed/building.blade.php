1bace67b686f53bcb93660d58c9de7cf891ca5dc
Ben <git@othyn.com>
2019-01-12 16:07:56 +0000

    Add view for building pages

---
+++ resources/views/fixed/building.blade.php
@@ -0,0 +1,18 @@
+@extends ('layouts.master')
+
+@section ('main-content')
+
+<div class="container-fluid h-100">
+    <div class="row h-100">
+        <div class="col-12 h-100 d-flex flex-column justify-content-center text-center">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="animated fadeInDown">Applying the finishing touches!</h1>
+                    <a class="d-block animated fadeInUp" href="/">Take me home</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+@endsection
