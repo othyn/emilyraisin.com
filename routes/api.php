a6e2b5bea4fd82b96129e97b20bdc37b035a8a1f
Ben <git@othyn.com>
2019-02-25 20:27:57 +0000

    Finalise endpoint functionality for contact form

--- routes/api.php
+++ routes/api.php
@@ -14,5 +14,24 @@ use Illuminate\Http\Request;
 */
 
 Route::middleware('throttle:10,1')->post('/contact', function (Request $request) {
-    return;
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email',
+        'message' => 'required|string|max:5000',
+        'captcha' => 'required|captcha',
+    ]);
+
+    $name = filter_var($request->name, FILTER_SANITIZE_STRING);
+    $email = filter_var($request->email, FILTER_SANITIZE_STRING);
+    $message = filter_var($request->message, FILTER_SANITIZE_STRING);
+
+    $to = env('CONTACT_EMAIL', '');
+
+    if (!empty($to)) {
+
+        $subject = "[emilyraisin.com] New contact form from: {$name} ({$email})";
+        $message = "Name: {$name}<br>Email: {$email}<br>Message:<br>{$message}";
+
+        mail($to, $subject, $message);
+    }
 });
