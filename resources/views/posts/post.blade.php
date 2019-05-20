1a418f945e19505761076960011f09b6b71cf6fd
Ben <git@othyn.com>
2019-05-20 23:14:33 +0100

    Post username dynamic based on join

--- resources/views/posts/post.blade.php
+++ resources/views/posts/post.blade.php
@@ -4,7 +4,7 @@
             {{ $post->title }}
         </a>
     </h2>
-    <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }} by <a href="/about">Emily</a></p>
+    <p class="blog-post-meta">{{ $post->created_at->toFormattedDateString() }} by {{ $post->user->name }}</p>
 
     {{ $post->subtitle }}
 
--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -5,7 +5,7 @@
     <div class="row">
         <div class="col-12">
             <h1 class="type-me">{{ $post->title }}</h1>
-            <h5 class="text-muted">{{ $post->created_at->toFormattedDateString() }} by <a href="/about">Emily</a></h5>
+            <h5 class="text-muted">{{ $post->created_at->toFormattedDateString() }} by {{ $post->user->name }}</h5>
         </div>
     </div>
 
