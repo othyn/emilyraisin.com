4a6ddaef858dd15801eef25687c9f5099ebb998c
Ben <git@othyn.com>
2019-05-20 22:01:02 +0100

    Store user against a post

--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -4,6 +4,7 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Post::class, function (Faker $faker) {
     return [
+        'user_id' => App\User::all()->random()->id,
         'title' => $faker->sentence,
         'subtitle' => $faker->sentences(5, true),
         'body' => $faker->paragraphs(15, true),
