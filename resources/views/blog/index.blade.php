47f3ea1b9f54e031433b47fc74cb4bc739b4541f
Ben <git@othyn.com>
2019-05-24 00:29:27 +0100

    Show tag on index if available

--- resources/views/blog/index.blade.php
+++ resources/views/blog/index.blade.php
@@ -4,8 +4,13 @@
 
     <div class="row">
         <div class="col-12">
-            <h1 class="type-me">Emily's Blog</h1>
-            <h5 class="text-muted">Making the words count.</h5>
+            @if (isset($tag))
+                <h1 class="type-me">{{ $tag->name }}</h1>
+                <h5 class="text-muted">Filtered by tag.</h5>
+            @else
+                <h1 class="type-me">Emily's Blog</h1>
+                <h5 class="text-muted">Making the words count.</h5>
+            @endif
         </div>
     </div>
 
