2bba2596d741a306748291e44d037cee3722a012
Ben <git@othyn.com>
2019-05-24 23:56:19 +0100

    Lock down tag methods

--- app/Http/Controllers/TagsController.php
+++ app/Http/Controllers/TagsController.php
@@ -8,6 +8,16 @@ use Illuminate\Http\Request;
 class TagsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth.admin')->except(['index']);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
