54a2cf8c66a9a037d999a021275651c409f91df8
Ben <git@othyn.com>
2019-05-25 00:21:05 +0100

    Allow mass assignment of name property on tag

--- app/Tag.php
+++ app/Tag.php
@@ -7,6 +7,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Tag extends Model
 {
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+    ];
+
     public function getRouteKeyName()
     {
         return 'name';
