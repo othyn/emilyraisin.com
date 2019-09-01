613a8856a1e70890bceda385f49d1322d9dec829
Ben <git@othyn.com>
2019-09-01 14:25:40 +0100

    Always generate a test user as the first user, utilising method injection

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\User;
+use Faker\Generator as Faker;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -9,8 +11,18 @@ class UsersTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        factory(App\User::class, 5)->create();
+        User::create([
+            'name' => 'Test Admin',
+            'email' => 'test@example.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
+            'remember_token' => str_random(10),
+            'profile_image' => $faker->imageUrl(200, 200),
+            'is_admin' => true,
+        ]);
+
+        factory(User::class, 4)->create();
     }
 }
