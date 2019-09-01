ff3e77403956a9457d47a6a0dff6810493f75e16
Ben <git@othyn.com>
2019-09-01 16:44:25 +0100

    Setup the factory, may come back to this but will do for now

--- database/factories/FileFactory.php
+++ database/factories/FileFactory.php
@@ -3,10 +3,22 @@
 /* @var $factory \Illuminate\Database\Eloquent\Factory */
 
 use App\File;
+use App\User;
 use Faker\Generator as Faker;
 
 $factory->define(File::class, function (Faker $faker) {
+    static $fileInstanceIncrement = 0;
+
+    $fileInstanceIncrement++;
+    $fileName = "my_test_file_{$fileInstanceIncrement}.png";
+
     return [
-        //
+        'user_id' => User::all()->random()->id,
+        'original_name' => $fileName,
+        'name' => $fileName,
+        'path' => "/public/storage/{$fileName}",
+        'size' => round(1 + mt_rand() / mt_getrandmax() * (9999999999 - 1), 2),
+        'mime' => 'image/png',
+        'created_at' => $faker->dateTimeBetween($startDate = '-1 years', $endDate = 'now'),
     ];
 });
