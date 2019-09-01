0537d8d77f78eb7bbc02365f252f4a68ee9f03b5
Ben <git@othyn.com>
2019-09-01 21:22:47 +0100

    Flesh out the file request object with file upload, validation and storage

--- app/Http/Requests/FileRequest.php
+++ app/Http/Requests/FileRequest.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Requests;
 
+use App\File;
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Http\FormRequest;
 
 class FileRequest extends FormRequest
@@ -13,7 +15,7 @@ class FileRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return auth()->user()->is_admin;
     }
 
     /**
@@ -24,7 +26,35 @@ class FileRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'file' => 'required|file|image|mimes:jpeg,png,gif,webp|max:2048',
         ];
+    }
+
+    /**
+     * Upload and save the posted file.
+     *
+     * @return \App\File $file
+     */
+    public function upload()
+    {
+        $uploadedFile = $this->file('file');
+
+        $newFileName = time().'_'.Str::lower($uploadedFile->getClientOriginalName());
+
+        $uploadedFile->storeAs('uploads', $newFileName, 'public');
+
+        $file = new File();
+
+        $file->user_id = auth()->user()->id;
+        $file->original_name = $uploadedFile->getClientOriginalName();
+        $file->name = $newFileName;
+        $file->path = "uploads/{$newFileName}";
+        $file->size = $uploadedFile->getClientSize();
+        $file->mime = $uploadedFile->getClientMimeType();
+        // https://github.com/symfony/symfony/blob/3.0/src/Symfony/Component/HttpFoundation/File/UploadedFile.php
+
+        $file->save();
+
+        return $file;
     }
 }
