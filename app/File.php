38d72b530f05cdf651d9441a1512be9e2e1ebd5b
Ben <git@othyn.com>
2019-09-01 16:21:43 +0100

    Setup object properties for File object

--- app/File.php
+++ app/File.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model
 {
-    //
+    /*
+     * Enable soft deletes
+     */
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'user_id',
+        'original_name',
+        'name',
+        'overview',
+        'path',
+        'url',
+        'size',
+        'mime',
+    ];
 }
